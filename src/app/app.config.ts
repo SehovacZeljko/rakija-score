45376333fb5e24ddf415b01568bcdad3c17d1d7b
Zeljko_Sehovac <zeljko.sehovac@singularity.is>
2026-02-24 09:53:15 +0100

    Connect to firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
+import { firebaseConfig } from './config/firebase.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+  ],
 };
---
+++ src/app/config/firebase.config.ts
@@ -0,0 +1,11 @@
+import { FirebaseOptions } from '@angular/fire/app';
+
+export const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyCanp6NueHAx8_YPuDB4mlLS06fzVLv5lE",
+  authDomain: "rakija-score.firebaseapp.com",
+  projectId: "rakija-score",
+  storageBucket: "rakija-score.firebasestorage.app",
+  messagingSenderId: "100389550388",
+  appId: "1:100389550388:web:849dfc6cfa4156acd88d61",
+  measurementId: "G-R10KS99B42"
+};
