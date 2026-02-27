ab0e5ff632a0e107ae1d6f1d196f4bd4a59efe38
Zeljko_Sehovac <zeljko.sehovac@singularity.is>
2026-02-27 12:28:58 +0100

    Change nav bars for larg screen

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component, Input, inject } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 import { AuthService } from '../../services/auth.service';
+import { NavItem } from '../bottom-nav/bottom-nav.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   @Input({ required: true }) title!: string;
+  @Input() items: NavItem[] = [];
 
   protected readonly authService = inject(AuthService);
 
