d4fd108d5a1a18c4ba5e2baca407dd3a8b748666
Zeljko_Sehovac <zeljko.sehovac@singularity.is>
2026-03-05 11:26:45 +0100

    Move config to environment files

--- src/app/config/firebase.config.ts
+++ src/app/config/firebase.config.ts
@@ -1,11 +1,4 @@
 import { FirebaseOptions } from '@angular/fire/app';
+import { environment } from '../../environments/environment';
 
-export const firebaseConfig: FirebaseOptions = {
-  apiKey: 'AIzaSyCanp6NueHAx8_YPuDB4mlLS06fzVLv5lE',
-  authDomain: 'rakija-score.firebaseapp.com',
-  projectId: 'rakija-score',
-  storageBucket: 'rakija-score.firebasestorage.app',
-  messagingSenderId: '100389550388',
-  appId: '1:100389550388:web:849dfc6cfa4156acd88d61',
-  measurementId: 'G-R10KS99B42',
-};
+export const firebaseConfig: FirebaseOptions = environment.firebase;
