f4ee3155b08fbdccf5ffb321027049d0448f0499
Zeljko_Sehovac <zeljko.sehovac@singularity.is>
2026-03-09 09:55:00 +0100

    Add image placeholder to active festival banner

--- src/app/components/active-festival-banner/active-festival-banner.component.ts
+++ src/app/components/active-festival-banner/active-festival-banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
 
 import { FestivalContextService } from '../../services/festival-context.service';
 
@@ -6,6 +7,7 @@ import { FestivalContextService } from '../../services/festival-context.service'
   selector: 'app-active-festival-banner',
   templateUrl: './active-festival-banner.component.html',
   styleUrl: './active-festival-banner.component.scss',
+  imports: [LucideAngularModule],
 })
 export class ActiveFestivalBannerComponent {
   readonly ctx = inject(FestivalContextService);
